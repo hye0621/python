4c001a29c0db1d1a73ac78b01b0adcfa89b8ebe3
hye0621 <54703051+hye0621@users.noreply.github.com>
2019-08-30 17:00:34 +0900

    test3

---
+++ test3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:41:41 2019
+
+@author: hye
+"""
+#%%
+#안녕을 출력해봅시다.
+print('안녕')
+
+print('안녕'+'하세요')
+#%%
+#'안녕'+'하세요'
+
+print('안녕','하세요')
+
+
+
+
+
+print(5+3)
+print(1/4)
+
+
+print('5'/3)
+
+#%%
+print('5'+'3')
+#%%
+
+#%%
+print('5'*3)
+#%%
+
+#%%
+a='5'
+print(a)
+b='김치'
+milk='물'
+_car=153
+
+a = 33
+print = (a)
+#%%
+1a=1
+print(1a)
